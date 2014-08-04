0b4a19ca763cbec1fcdff369ac2fc5613a1a82cb
Cuong Pham <phammanhcuong@gmail.com>
2014-08-04 15:03:05 -0500

    Plot both time and frequency.

--- zk-smoketest/plot_latencies.sh
+++ zk-smoketest/plot_latencies.sh
@@ -8,14 +8,21 @@ if [ $# -eq 0 ]
 fi
 
 NRUNS=$1
-SUFFIX='_delay100ms_'
+SUFFIX='_delay5ms_'
 #SUFFIX=''
 
 echo "Ploting async..."
 ./parse_kvm_event.py -o rate_async_log${SUFFIX}.pdf -m 1 -M $NRUNS --latency_unit='sec' --log_scale latency_async${SUFFIX}
 ./parse_kvm_event.py -o rate_async${SUFFIX}.pdf -m 1 -M $NRUNS --latency_unit='sec' --ylim_top=50000 latency_async${SUFFIX}
 
+for i in $(seq 1 $NRUNS); do
+    ./plot_event_flows.py -o '_async'${SUFFIX}${i} -r 10 latency_async${SUFFIX}${i}
+done
 
 echo "Ploting sync..."
 ./parse_kvm_event.py -o rate_sync_log${SUFFIX}.pdf -m 1 -M $NRUNS --latency_unit='sec' --log_scale latency_sync${SUFFIX}
 ./parse_kvm_event.py -o rate_sync${SUFFIX}.pdf -m 1 -M $NRUNS --latency_unit='sec' --ylim_top=50000 latency_sync${SUFFIX}
+
+for i in $(seq 1 $NRUNS); do
+    ./plot_event_flows.py -o '_sync'${SUFFIX}${i} -r 10 latency_sync${SUFFIX}${i}
+done
